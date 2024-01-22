89c47553becb28ca85d96f64d1f55175d527cb8e
coker2019 <olabisi2k11@gmail.com>
2024-01-22 10:26:26 +0100

    update app.js/index file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import Room from './components/Room';
+// App.js
 
-const App = () => (
-  <Provider store={store}>
-    <Room />
-  </Provider>
-);
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Cookies from 'js-cookie';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Sidebar from './components/Sidebar';
+import { setToken } from './redux/userAuth/authSlice';
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    const username = Cookies.get('username');
+
+    if (token) {
+      dispatch(setToken({ username, token }));
+    }
+  }, [dispatch]);
+
+  return (
+    <div style={{ display: 'flex', width: '100%' }}>
+      <Sidebar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
