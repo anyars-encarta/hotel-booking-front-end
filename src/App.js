22a061063bbed5083d58f3a8af6195329af1cf7e
Anyars Yussif <anyarsencarta@gmail.com>
2024-01-23 12:38:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import SignUp from './components/SignUp';
 import Sidebar from './components/Sidebar';
 import { setToken } from './redux/userAuth/authSlice';
 import NewRoom from './components/NewRoom';
-import NewCategory from './components/NewCategory';
-import { fetchCategories, saveFormData } from './redux/rooms/actions';
 import { fetchCategories, saveFormData } from './redux/actions';
 import Category from './components/Category';
 import CategoryDetail from './components/CategoryDetail';
@@ -54,10 +52,8 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/" element={<Room categories={categories} />} />
-        <Route path="/new-room" element={<NewRoom categories={categories} handleFormSubmit={handleFormSubmit} />} />
-        <Route path="/new-category" element={<NewCategory />} />
         <Route path="/" element={<Category />} />
+        <Route path="/newroom" element={<NewRoom categories={categories} handleFormSubmit={handleFormSubmit} />} />
         <Route path="/show-rooms" element={<Category />} />
         <Route path="/category_details/:id" element={<CategoryDetail />} />
       </Routes>
