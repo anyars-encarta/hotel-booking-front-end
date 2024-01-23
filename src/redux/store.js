cf1faf17c674a9553edaaba320742baed31b18ed
anyars-encarta <anyarsencarta@gmail.com>
2024-01-23 15:11:55 +0000

    Remobe rootReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { configureStore } from '@reduxjs/toolkit';
 // import { logger } from 'redux-logger';
-import { combineReducers } from 'redux';
-import { roomReducer, roomCategoryReducer, userReducer } from './rooms/reducers';
+import { roomReducer, categoryReducer, userReducer } from './rooms/reducers';
 import authReducer from './userAuth/authSlice';
 import categoryReducer from './categories/categorySlice';
 import roomsReducer from './rooms/roomSlice';
-
-const rootReducer = combineReducers({
-  rooms: roomReducer,
-  categories: {
-    roomCategoryReducer,
-    categoryReducer,
-  },
-  user: userReducer,
-  auth: authReducer,
-});
 
 const store = configureStore({
   reducer: {
