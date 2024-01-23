7faf7984844b260d405f3a257e084f52a57bd3df
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-23 09:39:30 +0430

    add the rooms reducer to stor.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import roomReducer from './reducers';
 import categoryReducer from './categories/categorySlice';
+import roomsReducer from './rooms/roomSlice';
 
 const rootReducer = combineReducers({
   room: roomReducer,
   category: categoryReducer,
+  rooms: roomsReducer,
 });
 
 const store = configureStore({
