6ca0c02df4109e8b5cec0e00a56d928c9598b721
akorede <emiolasaheed1@gmail.com>
2024-01-23 17:26:08 +0100

    Set Reservation row proptypes

--- src/components/ReservationRow.jsx
+++ src/components/ReservationRow.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import { HiEllipsisVertical } from 'react-icons/hi2';
+import PropTypes from 'prop-types';
 import ReservationMenu from './ReservationMenu';
 
-const ReservationRow = ({ room: { name}, city, user }) => {
+const ReservationRow = ({ room: { name }, city, user: { username, email } }) => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <li className=" relative grid grid-col-[0.6fr,2fr,2.4fr,1.4fr,1fr,3.2rem] grid-cols-[1fr,1fr,1fr,0.2fr] text-center  place-items-center gap-4 items-start border-t border-gray-200 dark:border-[#374151] py-4">
       <span>{name}</span>
       <p className=" flex flex-col text-sm">
-        <span>{user.username}</span>
-        <span>{user.email}</span>
+        <span>{username}</span>
+        <span>{email}</span>
       </p>
       <p className=" flex flex-col text-sm">
         <span>
@@ -25,6 +26,17 @@ const ReservationRow = ({ room: { name}, city, user }) => {
       { showMenu && <ReservationMenu setShowMenu={setShowMenu} /> }
     </li>
   );
+};
+
+ReservationRow.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  city: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default ReservationRow;
