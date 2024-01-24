3f601df348096584fe4b8fd08b4f1c8c17b4b0c6
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-24 11:01:34 +0430

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,32 +5,37 @@ const Navbar = () => (
     <ul className=" space-y-3 ">
       <li className="px-3 py-2 hover:bg-blue-500 hover:text-white md:hidden">
         <NavLink className=" flex items-center gap-2">
-          <span>Logout</span>
+          <span className=" font-semibold">Logout</span>
         </NavLink>
       </li>
       <li className="px-3 py-2 hover:bg-blue-500 hover:text-white">
-        <NavLink to="dashboard" className=" flex items-center gap-2">
-          <span>Home</span>
-        </NavLink>
-      </li>
-      <li className="px-3 py-2 hover:bg-blue-500 hover:text-white">
-        <NavLink to="login" className=" flex items-center gap-2">
-          <span>Login</span>
+        <NavLink to="/" className=" flex items-center gap-2">
+          <span className=" font-semibold">Home</span>
         </NavLink>
       </li>
       <li className="px-3 py-2 hover:bg-blue-500 hover:text-white">
         <NavLink to="show-rooms" className=" flex items-center gap-2">
-          <span>Rooms</span>
+          <span className=" font-semibold">Rooms</span>
         </NavLink>
       </li>
       <li className="px-3 py-2 hover:bg-blue-500 hover:text-white">
         <NavLink to="newroom" className=" flex items-center gap-2">
-          <span>New room</span>
+          <span className=" font-semibold">New room</span>
+        </NavLink>
+      </li>
+      <li className="px-3 py-2 hover:bg-blue-500 hover:text-white">
+        <NavLink to="delete_room" className=" flex items-center gap-2">
+          <span className=" font-semibold">Delete room</span>
         </NavLink>
       </li>
       <li className="px-3 py-2 hover:bg-blue-500 hover:text-white">
         <NavLink to="reservations" className=" flex items-center gap-2">
           <span className=" font-semibold">Reservations</span>
+        </NavLink>
+      </li>
+      <li className="px-3 py-2 hover:bg-blue-500 hover:text-white">
+        <NavLink to="logout" className=" flex items-center gap-2">
+          <span className=" font-semibold">Logout</span>
         </NavLink>
       </li>
     </ul>
