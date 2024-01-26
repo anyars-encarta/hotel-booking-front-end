2f2fa53bd821375526a1816da045b668c8c114f7
akorede <emiolasaheed1@gmail.com>
2024-01-26 23:22:24 +0100

    Format reservations date

--- src/hooks/useCreateReservation.js
+++ src/hooks/useCreateReservation.js
@@ -1,21 +1,15 @@
-import { Navigate, redirect, useNavigate, useParams } from 'react-router-dom';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { CreateReservation } from '../services/reservationsApi';
 
-
 const useCreateReservation = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  console.log(id);
   const query = useQueryClient();
   const { mutate: reserveRoom, isPending } = useMutation({
     mutationFn: (data) => CreateReservation(data),
     onSuccess: () => {
-      console.log('Reserved');
       query.invalidateQueries({ active: true });
     },
     onError: (error) => {
-      console.log(error);
+      throw new Error(error.message);
     },
   });
 
