df2f4645e866616c23280f3df1d9ddc7a57f2160
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-29 21:55:56 +0430

    add fronted tests

--- src/__tests__/DeleteRoom.test.js
+++ src/__tests__/DeleteRoom.test.js
@@ -1,63 +1,90 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import renderer from 'react-test-renderer';
+import { useSelector, useDispatch } from 'react-redux';
 import DeleteRoom from '../components/DeleteRoom';
 
-const mockStore = configureStore([]);
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(),
+}));
 
-describe('DeleteRoom component', () => {
-  let store;
-  beforeEach(() => {
-    store = mockStore({
-      rooms: {
-        rooms: [
-          { id: 1, name: 'Room 1', category_id: 1 },
-          { id: 2, name: 'Room 2', category_id: 2 },
-        ],
-        loading: false,
-        error: null,
-      },
-      category: {
-        categories: [
-          { id: 1, name: 'Category 1', price: 100 },
-          { id: 2, name: 'Category 2', price: 150 },
-        ],
-        loading: false,
-        error: null,
-      },
-    });
+describe('Tests DeleteRoom component', () => {
+  it('should render loading spinner when loading is true', () => {
+    const loading = true;
+    const error = null;
+    const dispatchMock = jest.fn();
+
+    useSelector.mockImplementation((selector) => selector({
+      rooms: { rooms: [], loading, error },
+      category: { categories: [] },
+    }));
+
+    useDispatch.mockReturnValue(dispatchMock);
+
+    const DeleteRoomComponent = renderer.create(<DeleteRoom />);
+    expect(DeleteRoomComponent.toJSON()).toMatchSnapshot();
   });
 
-  it('renders correctly', () => {
-    const { asFragment } = render(
-      <Provider store={store}>
-        <DeleteRoom />
-      </Provider>,
-    );
-    expect(asFragment()).toMatchSnapshot();
+  it('should render error message when error is present', () => {
+    const loading = false;
+    const error = 'Something went wrong!';
+    const dispatchMock = jest.fn();
+
+    useSelector.mockImplementation((selector) => selector({
+      rooms: { rooms: [], loading, error },
+      category: { categories: [] },
+    }));
+
+    useDispatch.mockReturnValue(dispatchMock);
+
+    const DeleteRoomComponent = renderer.create(<DeleteRoom />);
+    expect(DeleteRoomComponent.toJSON()).toMatchSnapshot();
   });
 
-  it('dispatches delete action on button click', async () => {
-    const { getAllByText } = render(
-      <Provider store={store}>
-        <DeleteRoom />
-      </Provider>,
-    );
+  it('should render table with room data when rooms and categories are present', () => {
+    const rooms = [
+      { id: 1, name: 'Room 1', category_id: 1 },
+      { id: 2, name: 'Room 2', category_id: 2 },
+    ];
+    const categories = [
+      { id: 1, name: 'Category 1', price: 100 },
+      { id: 2, name: 'Category 2', price: 200 },
+    ];
+    const loading = false;
+    const error = null;
+    const dispatchMock = jest.fn();
 
-    // Mock dispatch
-    store.dispatch = jest.fn();
+    useSelector.mockImplementation((selector) => selector({
+      rooms: { rooms, loading, error },
+      category: { categories },
+    }));
 
-    // Find the delete button within the first row (for example)
-    const deleteButtons = getAllByText(/Delete/i);
+    useDispatch.mockReturnValue(dispatchMock);
 
-    const deleteButton = deleteButtons[0];
-    // Simulate delete button click
-    fireEvent.click(deleteButton);
+    const DeleteRoomComponent = renderer.create(<DeleteRoom />);
+    expect(DeleteRoomComponent.toJSON()).toMatchSnapshot();
+  });
 
-    // Wait for dispatch and listRooms to be called
-    await waitFor(() => {
-      expect(store.dispatch).toHaveBeenCalledTimes(2); // deleteRoom + listRooms
-    });
+  it('should render no rooms available message when rooms is empty', () => {
+    const rooms = [];
+    const categories = [
+      { id: 1, name: 'Category 1', price: 100 },
+      { id: 2, name: 'Category 2', price: 200 },
+    ];
+    const loading = false;
+    const error = null;
+    const dispatchMock = jest.fn();
+
+    useSelector.mockImplementation((selector) => selector({
+      rooms: { rooms, loading, error },
+      category: { categories },
+    }));
+
+    useDispatch.mockReturnValue(dispatchMock);
+
+    const DeleteRoomComponent = renderer.create(<DeleteRoom />);
+    expect(DeleteRoomComponent.toJSON()).toMatchSnapshot();
   });
 });
